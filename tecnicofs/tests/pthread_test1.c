e63032f166b2076a212dcd512a0ddc410774a33b
Stefan Knutsen <stefan.knutsen@tecnico.ulisboa.pt>
2022-01-14 15:27:01 +0000

    Todos os testes finalizados

--- tecnicofs/tests/pthread_test1.c
+++ tecnicofs/tests/pthread_test1.c
@@ -6,14 +6,20 @@
 #include <unistd.h>
 #include <pthread.h>
 
+#define COUNT 10
+#define SIZE 21
+
+/* 
+ * This test creates two threads that write 10 characters in a cicle.
+ * Then copies to a external file the writen characters.
+*/
 
 void* thr_func_a(void* arg){
 
 	int f = *((int*)arg);
 	char* a = "a";
-	for (int i = 0; i < 10; i++){
-		tfs_write(f, a, strlen(a));
-	}
+	for (int i = 0; i < COUNT; i++)
+		assert(tfs_write(f, a, 1) == 1);
 
 	return NULL;
 }
@@ -21,9 +27,8 @@ void* thr_func_a(void* arg){
 void* thr_func_b(void* arg){
 	int f = *((int*)arg);
 	char* b = "b";
-	for (int i = 0; i < 10; i++){
-		tfs_write(f, b, strlen(b));
-	}
+	for (int i = 0; i < COUNT; i++)
+		assert(tfs_write(f, b, 1) == 1);
 
 	return NULL;
 }
@@ -31,39 +36,43 @@ void* thr_func_b(void* arg){
 
 int main(){
 
-	assert(tfs_init() != -1);
 	char *path = "/f1";
-	int f = tfs_open(path, TFS_O_CREAT);
-
+	char *path2 = "external_file.txt";
 	pthread_t tid[2];
 
-	pthread_create(&tid[0], NULL, thr_func_a, (void*)&f);
-	pthread_create(&tid[1], NULL, thr_func_b, (void*)&f);
+	assert(tfs_init() != -1);
+	int f = tfs_open(path, TFS_O_CREAT);
+	assert(f != -1);
 
-	pthread_join(tid[0], NULL);
-	pthread_join(tid[1], NULL);
+	assert(pthread_create(&tid[0], NULL, thr_func_a, (void*)&f) == 0);
+	assert(pthread_create(&tid[1], NULL, thr_func_b, (void*)&f) == 0);
+	assert(pthread_join(tid[0], NULL) == 0);
+	assert(pthread_join(tid[1], NULL) == 0);
 
-	char buffer[21];
-
-	tfs_close(f);
+	assert(tfs_close(f) != -1);
 	f = tfs_open(path, 0);
+	assert(f != -1);
 
-	tfs_read(f, buffer, sizeof(buffer));
+	char buffer[SIZE], str_aux1[SIZE], str_aux2[SIZE];
+	assert(tfs_read(f, buffer, SIZE) == SIZE);
 
-	char str_a[11];
-	char str_b[11];
-	memset(str_a, 'a', 10);
-	memset(str_b, 'b', 10);
+	/* Creates two strings that represent the expected reading. */
+	memset(str_aux1, 'a', COUNT);
+	memset(str_aux1 + COUNT, 'b', COUNT);
+	memset(str_aux2, 'b', COUNT);
+	memset(str_aux2 + COUNT, 'a', COUNT);
 
-	char str_aux1[21];
-	char str_aux2[21];
-	strcpy(str_aux1, str_a);
-	strcat(str_aux1, str_b);
-	strcpy(str_aux2, str_b);
-	strcat(str_aux2, str_a);
 	assert(strcmp(buffer, str_aux1) == 0 || strcmp(buffer, str_aux2) == 0);
 
     assert(tfs_close(f) != -1);
+	
+	/* Copies to external file. */
+	assert(tfs_copy_to_external_fs(path, path2) != -1);
+	FILE *fp = fopen(path2, "r");
+	assert(fp != NULL);
+	assert(fread(buffer, 1, sizeof(buffer), fp) == sizeof(buffer));
+	assert(strcmp(buffer, str_aux1) == 0 || strcmp(buffer, str_aux2) == 0);
+	assert(fclose(fp) != -1);
 	
     printf("Successful test.\n");
 	return 0;
--- tecnicofs/tests/pthread_test3.c
+++ tecnicofs/tests/pthread_test3.c
@@ -1,5 +1,3 @@
-// Fazer escrita uma vez e fazer leitura com dois processos ao mesmo tempo. Comparar as duas leituras no final. 
-
 #include "../fs/operations.h"
 #include <assert.h>
 #include <string.h>
@@ -8,41 +6,49 @@
 #include <unistd.h>
 #include <pthread.h>
 
-//char* a = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+#define SIZE 1000
+#define COUNT 500
 
-
+/* 
+ * This test writes a 1000 characters in a new file, 
+ * then creates 500 threads that open, read and 
+ * close the file.
+ */
 void* thr_func(void* arg){
 
 	char* a = (char*) arg;
 	char *path = "/f1";
+	char buffer[SIZE + 1];
+
 	int f = tfs_open(path, 0);
-	char buffer[1001];
-	tfs_read(f, buffer, 1000);
+	assert(f != -1);
+	assert(tfs_read(f, buffer, SIZE) == SIZE);
 	assert(strcmp(buffer, a) == 0);
-	tfs_close(f);
+	assert(tfs_close(f) != -1);
 
 	return NULL;
 }
 
 int main(){
 	
-	pthread_t tid[300];
+	pthread_t tid[COUNT];
 	assert(tfs_init() != -1);
 
 	char *path = "/f1";
 	int f = tfs_open(path, TFS_O_CREAT);
+	assert(f != -1);
 
-	char a[1001];
-	memset(a, 'A', 1000);
+	char a[SIZE];
+	memset(a, 'A', SIZE);
 
-	tfs_write(f, a, strlen(a));
-	tfs_close(f);
+	assert(tfs_write(f, a, SIZE) == SIZE);
+	assert(tfs_close(f) != -1);
 
-	for (int i = 0; i < 300; i++)
-		pthread_create(&tid[i], NULL, thr_func, (void*) &a);
+	for (int i = 0; i < COUNT; i++)
+		assert(pthread_create(&tid[i], NULL, thr_func, (void*) &a) == 0);
 
-	for (int i = 0; i < 300; i++)
-		pthread_join(tid[i], NULL);
+	for (int i = 0; i < COUNT; i++)
+		assert(pthread_join(tid[i], NULL) == 0);
 
     printf("Successful test.\n");
 	return 0;
